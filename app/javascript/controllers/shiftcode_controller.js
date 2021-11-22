a28cec5ba24102ca1f43841e45f7f618a34af311
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-11-22 13:52:32 +0700

    อัพเดทจำนวนพนักงานเมื่อถูก assign ot ตาม shiftcode

--- app/javascript/controllers/shiftcode_controller.js
+++ app/javascript/controllers/shiftcode_controller.js
@@ -1,14 +1,17 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
-    static targets = ["actualCount"]
+    static targets = ["actualCount","planCount"]
     static values = { pagename: String }
     connect() {
         this.tableName = "งานถอนขน"
     }
     select_shiftcode(event) {
         this.actual_count = 0
+        this.plan_count = 0
         this.table = document.querySelector("#" + this.tableName + "_c")
+        this.planTable = document.querySelector("#" + this.tableName + "_p")
         this.val = event.currentTarget.value
+        //actual time
         Array.from(this.table.rows).forEach((each_row) => {
             each_row.style.display = "table-row";
             this.row_with_time_select = each_row.querySelectorAll(`td.time_${this.val}`)
@@ -20,6 +23,14 @@ export default class extends Controller {
             }
         })
         this.actualCountTarget.textContent = this.actual_count
+        //plan time
+        Array.from(this.table.rows).forEach((each_row) => {
+            this.plan_row_with_time_select = each_row.querySelectorAll(`td.plan_${this.val}`)
+            if(this.plan_row_with_time_select.length>0){
+                this.plan_count += 1
+            }
+        })
+        this.planCountTarget.textContent = this.plan_count
     }
     set_table_from_page() {
         this.tableName = this.pagenameValue
