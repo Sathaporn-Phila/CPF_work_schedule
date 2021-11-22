2f3ce55946af06ab9240b7ffcc89b6a2280dc89c
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-11-22 12:07:06 +0700

    เมื่อเลือกกะ จะแสดงเฉพาะกะเลือกและอัพเดทเลขเฉพาะกะ

--- app/javascript/controllers/shiftcode_controller.js
+++ app/javascript/controllers/shiftcode_controller.js
@@ -1,20 +1,25 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
+    static targets = ["actualCount"]
     static values = { pagename: String }
     connect() {
         this.tableName = "งานถอนขน"
     }
     select_shiftcode(event) {
+        this.actual_count = 0
         this.table = document.querySelector("#" + this.tableName + "_c")
         this.val = event.currentTarget.value
-        this.filter_element = document.querySelectorAll(`.time_${this.val}`)
-        console.log(this.table.rows)
         Array.from(this.table.rows).forEach((each_row) => {
-            each_row.style.display = "block";
-            if (!(Array.from(this.filter_element).includes(each_row))) {
+            each_row.style.display = "table-row";
+            this.row_with_time_select = each_row.querySelectorAll(`td.time_${this.val}`)
+            if (this.row_with_time_select.length == 0){
                 each_row.style.display = "none";
             }
+            else{
+                this.actual_count += 1
+            }
         })
+        this.actualCountTarget.textContent = this.actual_count
     }
     set_table_from_page() {
         this.tableName = this.pagenameValue
